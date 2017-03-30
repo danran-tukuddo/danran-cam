faaf5706c207429df9b053dcd9d0de0e9b86c470
古川信行 <t_sakura@m1.ffn.ne.jp>
2017-03-31 06:59:48 +0900

    カメラSampleコードを追加

--- app.js
+++ app.js
@@ -36,10 +36,7 @@ class App{
     //ブラウザ(Chromium)の起動, 初期画面のロード
     this.mainWindow = new electron.BrowserWindow({width: 800, height: 600});
     //this.mainWindow.maximize();
-    //this.mainWindow.loadURL(config.FIREBASE_APP_URL);
-    //this.mainWindow.loadURL("file://" + __dirname + "/public/app.html");
-
-    this.mainWindow.loadURL("chrome://settings");
+    this.mainWindow.loadURL(config.FIREBASE_APP_URL);
     this.mainWindow.on('closed', function(){
       console.log("closed");
       electron.session.defaultSession.clearCache(() => {});
