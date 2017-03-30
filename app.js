0678374730770d8aae0c668e12bd0dc0c179b2f7
古川信行 <t_sakura@m1.ffn.ne.jp>
2017-03-30 23:02:13 +0900

    UV4L利用に変更

--- app.js
+++ app.js
@@ -32,14 +32,7 @@ class App{
     //ブラウザ(Chromium)の起動, 初期画面のロード
     this.mainWindow = new electron.BrowserWindow({width: 800, height: 600});
     //this.mainWindow.maximize();
-    this.mainWindow.loadURL(config.FIREBASE_URL);
-    this.mainWindow.on('closed', this.windowClosed);
-  }
-
-  //Windowを閉じた時
-  windowClosed(){
-    console.log("closed");
-    this.mainWindow = null;
+    this.mainWindow.loadURL(config.UV4L_SERVER_URL);
   }
 }
 
