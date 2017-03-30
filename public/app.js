3f8f53154ae20f1e5d7b1b07b625e56a61b51969
古川信行 <t_sakura@m1.ffn.ne.jp>
2017-03-31 01:05:42 +0900

    カメラSampleコードを追加

--- public/app.js
+++ public/app.js
@@ -2,14 +2,16 @@
 
 var video = document.getElementById("video");
 
-navigator.webkitGetUserMedia({video: true, audio: false}, _handleSuccess, _handleError);
+navigator.mediaDevices.getUserMedia({video: true, audio: false}, _handleSuccess, _handleError);
 
 function _handleSuccess(localMediaStream) {
+  console.log("_handleSuccess");
   video.style.display = "block";
   video.src = window.URL.createObjectURL(localMediaStream);
   video.play();
 }
 
 function _handleError() {
+  console.log("_handleError");
   alert("ERROR: カメラを起動できませんでした。");
 }
