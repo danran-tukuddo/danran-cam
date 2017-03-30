f6d3e1f81bec94fe2797f8b892e2a92ef02b16f5
古川信行 <t_sakura@m1.ffn.ne.jp>
2017-03-30 22:28:38 +0900

    config.jsを追加

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 class Config{
-  //uv4l --driver uvc --device-id 046d:0825 --config-file=/etc/uv4l/uv4l-uvc.conf
   //コンストラクタ
   constructor(){
     console.log("constructor");
     this.UV4L_DEVICE_ID = "046d:0825";
-    this.FIREBASE_URL = "http://localhost:5000";
+    //this.FIREBASE_URL = "http://localhost:5000";
+    this.FIREBASE_URL = "https://danran-5202a.firebaseapp.com";
   }
 }
 
