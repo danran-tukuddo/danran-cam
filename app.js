82861a64296ece23ba3ceba3eda18353b1c2f288
古川信行 <t_sakura@m1.ffn.ne.jp>
2017-03-31 03:06:37 +0900

    カメラSampleコードを追加

--- app.js
+++ app.js
@@ -37,7 +37,9 @@ class App{
     this.mainWindow = new electron.BrowserWindow({width: 800, height: 600});
     //this.mainWindow.maximize();
     //this.mainWindow.loadURL(config.FIREBASE_APP_URL);
-    this.mainWindow.loadURL("file://" + __dirname + "/public/app.html");
+    //this.mainWindow.loadURL("file://" + __dirname + "/public/app.html");
+
+    this.mainWindow.loadURL("https://appr.tc/");
     this.mainWindow.on('closed', function(){
       console.log("closed");
       electron.session.defaultSession.clearCache(() => {});
