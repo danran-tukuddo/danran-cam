1306f01eddbe2bb2d93cb890e324f8d86bec3548
古川信行 <t_sakura@m1.ffn.ne.jp>
2017-03-30 23:16:38 +0900

    localhostに変更

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ class Config{
   constructor(){
     console.log("constructor");
     this.UV4L_DEVICE_ID = "046d:0825";
-    this.UV4L_SERVER_URL = "http://192.168.10.6:8090/stream/webrtc";
+    this.UV4L_SERVER_URL = "http://127.0.0.1:8090/stream/webrtc";
   }
 }
 
