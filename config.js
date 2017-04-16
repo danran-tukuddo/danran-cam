a95b9e6df549359681b8261f3b05169f6bf5d79a
古川信行 <t_sakura@m1.ffn.ne.jp>
2017-04-16 19:38:57 +0900

    URL変更テスト

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ class Config{
   //コンストラクタ
   constructor(){
     console.log("constructor");
-    this.FIREBASE_APP_URL = "https://danran-5202a.firebaseapp.com/cam.html";
-    //this.FIREBASE_APP_URL = "https://web-designer.cman.jp/javascript_ref/window/size/";
+    //this.FIREBASE_APP_URL = "https://danran-5202a.firebaseapp.com/cam.html";
+    this.FIREBASE_APP_URL = "https://web-designer.cman.jp/javascript_ref/window/size/";
   }
 }
 
