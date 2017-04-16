6afd7d1e5a5bd27914ff1ef81b746f9d2fef409f
古川信行 <t_sakura@m1.ffn.ne.jp>
2017-04-16 19:58:42 +0900

    フルスクリーン化

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ class App{
     //ブラウザ(Chromium)の起動, 初期画面のロード
     //QVGA 1280x960
     //XVGA 1024x768
-    this.mainWindow = new electron.BrowserWindow({width:1024,height:768,center: true,"always-on-top": true, kiosk: true, frame:false, fullscreen: true});
+    this.mainWindow = new electron.BrowserWindow({width:1024,height:768,center: true,"always-on-top": true, kiosk: true, frame:false, fullscreen: true,useContentSize: true});
     //this.mainWindow = new electron.BrowserWindow({width:1024,height:768,center: true,"always-on-top": true});
     this.mainWindow.maximize();
     this.mainWindow.setFullScreen(true);
--- config.js
+++ config.js
@@ -3,7 +3,6 @@ class Config{
   constructor(){
     console.log("constructor");
     this.FIREBASE_APP_URL = "https://danran-5202a.firebaseapp.com/cam.html";
-    //this.FIREBASE_APP_URL = "https://web-designer.cman.jp/javascript_ref/window/size/";
   }
 }
 
